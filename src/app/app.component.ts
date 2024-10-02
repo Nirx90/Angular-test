508451a2b40b77e5a596cffd542399efa36928fb
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-10-02 16:34:00 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'test';
+  dvalue:string='';
+  getInput(val:string){
+    console.warn(val)
+    this.dvalue =val;
+  }
+}
